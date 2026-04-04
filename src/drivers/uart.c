a536f1f3ae423d25d254c2a3127f7387aac18273
Shawn Theo Moses <shawnmoses1847@gmail.com>
2026-04-04 10:48:06 +0200

    Untested DMA UART Driver.

--- src/drivers/uart.c
+++ src/drivers/uart.c
@@ -1,5 +1,8 @@
 #include "../../lib/stm32f401xe.h"
 
+#include "../lib/include/string.h"
+#include <stdint.h>
+
 #define UART_TX_GPIO 2
 #define UART_RX_GPIO 3
 #define UART_AF 7
@@ -9,37 +12,19 @@
 
 void uart_init(unsigned baud_rate) {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-    // Peripheral bus
+    RCC->AHB1ENR |= RCC_AHB1ENR_DMA1EN;
     RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
 
-    // set GPIO A to enable UART_TX
-    GPIOA->MODER &= ~(3 << (UART_TX_GPIO * 2));
-    GPIOA->MODER |= (2 << (UART_TX_GPIO * 2));
+    GPIOA->MODER &= ~(GPIO_MODER_MODER2 | GPIO_MODER_MODER3);
+    GPIOA->MODER |= (GPIO_MODER_MODER2_1 | GPIO_MODER_MODER3_1);
 
-    // set GPIO A to enable UART_RX
-    GPIOA->MODER &= ~(3 << (UART_RX_GPIO * 2));
-    GPIOA->MODER |= (2 << (UART_RX_GPIO * 2));
-
-    // set Alternate Function PA3 of UART_RX
-    GPIOA->AFR[0] &= ~(0xF << (UART_RX_GPIO * 4));
-    GPIOA->AFR[0] |= (UART_AF << (UART_RX_GPIO * 4));
-
-    // set Alternate Function PA2 of UART_TX
-    GPIOA->AFR[0] &= ~(0xF << (UART_TX_GPIO * 4));
-    GPIOA->AFR[0] |= (UART_AF << (UART_TX_GPIO * 4));
+    GPIOA->AFR[0] |= (0x07 << GPIO_AFRL_AFSEL2_Pos) | (0x07 << GPIO_AFRL_AFSEL3_Pos);
 
     USART2->BRR = PERIPHERAL_CLOCK / baud_rate;
+    USART2->CR1 = USART_CR1_TE_Msk; // Will take care of recieving later..
+    USART2->CR1 |= USART_CR1_UE_Msk;
 
-    // set the word length to 8 bit
-    USART2->CR1 &= ~USART_CR1_M;
-    USART2->CR1 &= ~USART_CR1_PCE;
-
-    // set 1 stop bit
-    USART2->CR2 &= ~(0x3 << (12));
-
-    // enable TE, RE, UE
-    USART2->CR1 |= (1 << 3) | (1 << 2);
-    USART2->CR1 |= (1 << 13);
+    USART2->CR3 |= USART_CR3_DMAT_Msk;
 }
 
 void uart_write_char(char c) {
@@ -49,12 +34,21 @@ void uart_write_char(char c) {
 }
 
 void uart_write_string(const char* s) {
-    const char *c = s;
-    while (*c != '\0') {
-        if (*c == '\n') uart_write_char('\r');
-        uart_write_char(*c);
-        c++;
-    }
+    int len = strlen(s);
+
+    while (DMA1_Stream6->CR & DMA_SxCR_EN);
+
+    DMA1_Stream6->CR = 0;
+    DMA1->HIFCR |= (0x3f << 16);
+    USART2->SR &= ~USART_SR_TC;
+
+    DMA1_Stream6->M0AR = (uint32_t)s;
+    DMA1_Stream6->NDTR = len;
+    DMA1_Stream6->PAR = (uint32_t)&(USART2->DR);
+
+    DMA1_Stream6->CR = (4 << DMA_SxCR_CHSEL_Pos) | DMA_SxCR_MINC_Msk | DMA_SxCR_DIR_0 | DMA_SxCR_TCIE_Msk;
+
+    DMA1_Stream6->CR |= DMA_SxCR_EN_Msk;
 }
 
 void uart_write_buf(const char* s, int len) {
